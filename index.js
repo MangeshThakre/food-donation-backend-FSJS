a4f679cbb619f4ffa85e3dc4d6ed6ff73bf7b4d7
Mangesh Thakre <mangeshthakre933@gmail.com>
2023-01-31 17:41:11 +0530

    remove socket

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 require("dotenv").config();
 require("./config/database");
 const app = require("./app.js");
-const socketIO = require("socket.io");
-const socketCallback = require("./socket/socket.js");
+// const socketIO = require("socket.io");
+// const socketCallback = require("./socket/socket.js");
 
 const server = app.listen(process.env.PORT, () =>
   console.log(`server listning at http://localhost:${8081}`)
 );
 
+// serverless not support socket.io
 // run socket on server
-const io = socketIO(server, { cors: { origin: "*" } });
-
-const connectedUsers = {};
-
-io.on("connection", (socket) => {
-  socketCallback(connectedUsers, socket);
-});
+// const io = socketIO(server, { cors: { origin: "*" } });
+// const connectedUsers = {};
+// io.on("connection", (socket) => {
+//   socketCallback(connectedUsers, socket);
+// });
